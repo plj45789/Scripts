c098f8d87872664bfd15943f5754e98336129f38
plj45789 <Jackson.Paul345@Gmail.com>
2017-02-15 16:38:59 -0500

    first published
    first published

---
+++ EPG/FetchEPG.cmd
@@ -0,0 +1,5 @@
+@echo off
+set user="plj49876"
+set pass="XA9zcyS5s5RFudBzjAjD"
+cd C:\Users\skp12\Documents\batch jobs\EPG
+"C:\Users\skp12\Documents\batch jobs\EPG\mc2xml" -J %user%^^:%pass% -d 336 -b -c us -g 02155 -o "C:\Users\skp12\Documents\batch jobs\EPG\tvguide.xml"
